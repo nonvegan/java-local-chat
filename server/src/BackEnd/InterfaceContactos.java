a73b06e619abeb86da75c6733217e41e6d24e0fe
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2021-02-16 22:55:50 +0000

    Add files via upload

---
+++ server/src/BackEnd/InterfaceContactos.java
@@ -0,0 +1,15 @@
+package BackEnd;
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface InterfaceContactos extends Remote {
+
+    public void putContacto(User user) throws RemoteException;
+    
+    public ListaUsers getContactos() throws RemoteException;
+    
+    public ListaUsers getContactos(String stringProcura) throws RemoteException;
+
+}
